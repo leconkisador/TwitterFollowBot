7f4ef4d495b981e4a51cbb0551fb3ed99cd3e3af
Conkisador <jeremysterieux@gmail.com>
2024-04-05 04:00:38 +0200

    changing the follow method

--- script.js
+++ script.js
@@ -37,24 +37,23 @@ function randbot(nfollwer){
         }, getRandomDelay(10000));
         
         setTimeout(() => {
-            const following = document.querySelector("#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > div.css-175oi2r.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-175oi2r.r-1e5uvyk.r-6026j > div:nth-child(2) > nav > div > div.css-175oi2r.r-1adg3ll.r-16y2uox.r-1wbh5a2.r-1pi2tsx > div > div:last-child > a")
+            const following = document.querySelector("#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > div.css-175oi2r.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-175oi2r.r-1e5uvyk.r-6026j > div:nth-child(2) > nav > div > div.css-175oi2r.r-1adg3ll.r-16y2uox.r-1wbh5a2.r-1pi2tsx > div > div:nth-last-child(2) > a")
             following.click()
         }, getRandomDelay(12000));
 
         setTimeout(() => {
 
             function clickEverySecond(nfollwer) {
-                count=1
+                count=2
                 const interval = setInterval(() => {
                     const selector = `#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > section > div > div > div:nth-child(${count}) > div > div > div > div > div.css-175oi2r.r-1iusvr4.r-16y2uox > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wtj0ep > div.css-175oi2r.r-19u6a5r > div`;
                     //               "#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > section > div > div > div:nth-child(1) > div > div > div > div > div.css-175oi2r.r-1iusvr4.r-16y2uox > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wtj0ep > div.css-175oi2r.r-19u6a5r > div")
                     const element = document.querySelector(selector);
                     
 
-                    console.log(window.getComputedStyle(element).getPropertyValue('background-color')) ;
-                    
 
-                    if (element) {
+
+                    if (window.getComputedStyle(element).getPropertyValue('background-color')==="rgb(15, 20, 25)") {
                         element.click();
                         console.log(`Clicked on element ${count}`);
                         count++;
@@ -65,7 +64,7 @@ function randbot(nfollwer){
                     } else {
                         count++
                         console.error("L'élément n'a pas été trouvé.");
-                        clearInterval(interval); // Arrête l'intervalle si l'élément n'est pas trouvé
+                        
                     }
                 }, 1000); // Répète toutes les secondes (1000 millisecondes)
             }
